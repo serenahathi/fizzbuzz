56fbb117e205d9d44375b1ae6221d383c32e2576
Laura Chan <laurachan205@gmail.com>
2018-01-17 14:04:06 +0000

    modify fizzbuzz_spec.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,24 @@
+# def fizzbuzz(number)
+#   if number % 3 == 0 && number % 5 == 0
+#     return "fizzbuzz"
+#   elsif number % 3 == 0
+#     return "fizz"
+#   elsif number % 5 == 0
+#     return "buzz"
+#   else
+#     return number
+#   end
+# end
+
 def fizzbuzz(number)
-  "fizz"
+  case
+  when number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  when number % 3 == 0
+     "fizz"
+  when number % 5 == 0
+     "buzz"
+  else
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,27 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+
+  it 'returns number when passed non-fizzbizz numbers' do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(34)).to eq 34
+  end
+
+  it 'returns "fizzbuzz" when passed multiples of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
   end
 end
